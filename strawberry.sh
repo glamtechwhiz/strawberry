10a6a3fd4541871d8c152124f956ce3e691c0ae2
glamtechwhiz <emaahwani@gmail.com>
2024-07-30 16:43:24 +0700

    Update strawberry.sh

--- strawberry.sh
+++ strawberry.sh
@@ -1,1 +1,39 @@
+🔹Task-1:
 
+Codes: https://raw.githubusercontent.com/glamtechwhiz/strawberry/main/strawberry.sh
+
+git clone https://github.com/dkbroy
+
+cd name.sh
+
+chmod +x name.sh && ./name.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
+
+🔹Task-2: 
+
+Codes: https://github.com/dkbroyt/test/blob/main/test.sh
+
+git clone https://github.com/dkbroyt
+
+cd name.sh
+
+chmod +x name.sh
+
+. /name.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
+
+🔹Task-3: 
+
+Codes: https://github.com/dkbroyt/busy/blob/main/busy.sh
+
+git clone https://github.com/dkbroyt
+
+cd name
+
+chmod +x name.sh
+
+. /name.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
